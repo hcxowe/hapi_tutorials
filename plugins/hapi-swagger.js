b41b1f40dfce4e55022288cf52525ddaccf6113b
hcxowe <hcxowe@126.com>
2019-10-24 17:01:32 +0800

    使用package.json得version

--- plugins/hapi-swagger.js
+++ plugins/hapi-swagger.js
@@ -1,11 +1,12 @@
 const hapiSwagger = require('hapi-swagger')
+const package = require('../package')
 
 module.exports = {
     plugin: hapiSwagger,
     options: {
         info: {
             title: 'API Documentation',
-            version: '1.0',
+            version: package.version,
         }
     }
 }
